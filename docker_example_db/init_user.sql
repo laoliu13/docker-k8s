0cefd248bc703fbb122a19218e675f0863b08f11
liuzhijun <liuzhijun931030@gmail.com>
2023-08-27 16:15:30 +0800

    docker-compose 示例

---
+++ docker_example_db/init_user.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE `docker_example_db` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+
+use docker_example_db;
+-- ----------------------------
+-- Table structure for t_channel
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL,
+  `name` VARCHAR(36) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+insert into user values (1, '小张', 10);
+insert into user values (2, '小王', 11);
+insert into user values (3, '小刘', 14);
+insert into user values (4, '小李', 11);
+insert into user values (5, '小宋', 12);
+insert into user values (6, '小陈', 9);
+insert into user values (7, '小欧', 10);
--- src/main/resources/init_sql/init_user.sql
+++
@@ -1,7 +0,0 @@
-insert into user values (1, '小张', 10);
-insert into user values (1, '小王', 11);
-insert into user values (1, '小刘', 14);
-insert into user values (1, '小李', 11);
-insert into user values (1, '小宋', 12);
-insert into user values (1, '小陈', 9);
-insert into user values (1, '小欧', 10);
